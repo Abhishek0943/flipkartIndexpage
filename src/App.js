afb655956159ae2fe48995ce928902aa6fbdb7bc
abhishek pundir <100474102+Abhishek0943@users.noreply.github.com>
2024-03-21 10:42:22 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { FaAngleDown } from "react-icons/fa6";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { FaAngleRight } from "react-icons/fa6";
 function App() {
   const [isSidebar, setIsSidebar] = useState(false)
   const [value, setValue] = useState("")
@@ -24,7 +25,7 @@ function App() {
       <div className={styles.container}>
         <div className={styles.main} style={{ height: "70px" }}>
           <div className={styles.logo}>
-            <img src="https://1000logos.net/wp-content/uploads/2021/02/Flipkart-logo-500x281.png" style={{width:"100px", marginTop:"10px"}} alt="" />
+            <img src="https://1000logos.net/wp-content/uploads/2021/02/Flipkart-logo-500x281.png" style={{ width: "100px", marginTop: "10px" }} alt="" />
           </div>
           <div className={styles.search}>
             <IoSearch className={styles.searchIcon} />
@@ -47,7 +48,7 @@ function App() {
 
         </div>
       </div >
-      <div style={{ height: "calc(100vh - 70px)", background: "#e6e6e6", maxWidth: "100vw", paddingTop: "10px" }}>
+      <div style={{ background: "#e6e6e6", maxWidth: "100vw", paddingTop: "10px", paddingBottom:"70px" }}>
         <div style={{ display: "flex", flexDirection: "column", position: "relative", zIndex: "3" }}>
 
 
@@ -112,7 +113,7 @@ function App() {
           }
 
         </div>
-        <div className={styles.main} style={{ display: "block", marginTop:"20px" }}>
+        <div className={styles.main} style={{ display: "block", marginTop: "20px", width: "90% !important" }}>
           <Slider {...settings}>
 
             <div className="bannerImg">
@@ -124,6 +125,62 @@ function App() {
 
           </Slider>
           <div style={{ width: "100%" }}></div>
+        </div>
+        <div className={styles.main} style={{ display: "block", marginTop: "50px", background: "white", padding: "20px", borderRadius: "5px" }}>
+          <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <p style={{ fontSize: "1.5rem", fontWeight: "500" }}>Best of Electronics</p>  <div style={{ background: "blue", height: "30px", aspectRatio: "1", cursor: "pointer", display: "flex", alignItems: "center", justifyContent: "center", borderRadius: "50%" }}><FaAngleRight color="white" /></div>
+          </div>
+
+
+          <div className={`${styles.main} ${styles.section}`} style={{ margin: "20px 0px", alignItems: "center", backgroundColor: "white", borderRadius: "5px", display: "flex", justifyContent: "space-between", gap: "20px", alignItems: "center", overflowX: "auto" }}>
+            <div>
+              <img src="https://static-ecapac.acer.com/media/catalog/product/v/2/v227q_3_2__1.png?optimize=high&bg-color=255,255,255&fit=bounds&height=500&width=500&canvas=500:500&format=jpeg" alt="" />
+              <p>LED Monitor</p>
+              <p>$250</p>
+            </div>
+            <div>
+              <img src="https://in.canon/media/image/2023/02/06/d2a14a492cb94cbab491012c291fd6f5_EOS+R8+RF24-50mm+f4.5-6.3+IS+STM+Front+Slant.png" alt="" />
+              <p>Camera</p>
+              <p>$10.5</p>
+            </div>
+            <div>
+              <img src="https://5.imimg.com/data5/IR/AL/MY-7950336/viewsonic-dlp-projector-pa500s-500x500.jpg" alt="" />
+              <p>Projector</p>
+              <p>$16.12</p>
+            </div>
+            <div >
+              <img src="https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100" alt="" />
+              <p>Laptop</p>
+              <p>$32.34</p>
+            </div>
+            <div >
+              <img src="https://ke.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/54/1624431/1.jpg?7440" alt="" />
+              <p>Projector</p>
+              <p>$58</p>
+
+            </div>
+            <div>
+              <img src="https://5.imimg.com/data5/SELLER/Default/2021/3/PA/ZQ/LT/121439134/epson-colore-printer-inkjet-500x500.jpg" alt="" />
+              <p>Printer </p>
+              <p>$50</p>
+            </div>
+            <div>
+              <img src="https://ke.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/78/8635331/1.jpg?7680" alt="" />
+              <p>Monitor</p>
+              <p>$12.3</p>
+            </div>
+            <div>
+              <img src="https://m.media-amazon.com/images/I/618zZ7u3sUL._SX522_.jpg" alt="" />
+              <p>MageGee Portable</p>
+              <p>$49.99</p>
+            </div>
+            <div>
+              <img src="https://m.media-amazon.com/images/I/61XULBEea2L._SX522_.jpg" alt="" />
+              <p>Keychron K8 Pro</p>
+              <p>$94.99</p>
+            </div>
+
+          </div>
         </div>
       </div>
       {
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,43 +4,55 @@ import styles from "./Footer.module.css"
 function Footer() {
   return (
     <>
-      <div className={styles.container} style={{ width: "100%", minHeight: "100px", backgroundColor: "#313131" }}>
+      <div className={styles.container} style={{ width: "100%", minHeight: "100px", backgroundColor: "#313131", }}>
         <div className={styles.ul}>
           <h6>About</h6>
           <p>contact us</p>
+          <p>About us</p>
           <p>Careers</p>
-          <p>Careers</p>
-          <p>Careers</p>
-          <p>Careers</p>
+          <p>Flipkart Stories</p>
+          <p>Press</p>
         </div>
         <div className={styles.ul}>
-          <h6>About</h6>
-          <p>contact us</p>
-          <p>Careers</p>
-          <p>Careers</p>
-          <p>Careers</p>
-          <p>Careers</p>
+          <h6>Group Companies</h6>
+          <p>Myntra</p>
+          <p>Flipkart Wholesale</p>
+          <p>Cleartrip</p>
+          <p>Shopsy</p>
         </div><div className={styles.ul}>
-          <h6>About</h6>
-          <p>contact us</p>
-          <p>Careers</p>
-          <p>Careers</p>
-          <p>Careers</p>
-          <p>Careers</p>
+          <h6>Help</h6>
+          <p>payment</p>
+          <p>Shipping</p>
+          <p>Cancellation & Return
+          </p>
+          <p>FAQ</p>
+          <p>Report Infringment</p>
         </div><div className={styles.ul}>
-          <h6>About</h6>
-          <p>contact us</p>
-          <p>Careers</p>
-          <p>Careers</p>
-          <p>Careers</p>
-          <p>Careers</p>
+          <h6>Consumer Policy</h6>
+          <p>Cancellation & Return</p>
+          <p>Term of use</p>
+          <p>Security</p>
+          <p>Privacy</p>
+          <p>Sitemap</p>
+          <p>EPR Compliance</p>
         </div><div className={styles.ul}>
-          <h6>About</h6>
-          <p>contact us</p>
-          <p>Careers</p>
-          <p>Careers</p>
-          <p>Careers</p>
-          <p>Careers</p>
+          <h6>Mail us</h6>
+          <p>Flipkart Internet Private Limited, </p>
+          <p>Buildings Alyssa, Begonia & </p>
+          <p>Clove Embassy Tech Village, </p>
+          <p>Outer Ring Road, Devarabeesanahalli Village, </p>
+          <p>Bengaluru, 560103, </p>
+          <p>Karnataka, India</p>
+        </div>
+        <div className={styles.ul}>
+          <h6>Registered Office Address:</h6>
+          <p>Flipkart Internet Private Limited, </p>
+          <p>Buildings Alyssa, Begonia & </p>
+          <p>Clove Embassy Tech Village, </p>
+          <p>Outer Ring Road, Devarabeesanahalli Village, </p>
+          <p>Bengaluru, 560103, </p>
+          <p>Karnataka, India</p>
+          <p>CIN : U51109KA2012PTC066107 </p>
         </div>
       </div>
       <div style={{ background: "black", padding: "20px" }}>
